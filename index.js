a817f9dea563222a0e57f2f1ca0a2ede87613fc0
Marcone Silva de Brito <marconebritt@gmail.com>
2022-06-14 22:55:12 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function mudaCorFundo(){
+    const menu = document.querySelector('#coresFundo');
+    const cor = menu.options[menu.selectedIndex].value;
+    document.body.style.background = cor;
+}
